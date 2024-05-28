9baa85294900ea4e52cff2b8f808196d440f4e33
Joel Alvarado <107654806+Joey314159@users.noreply.github.com>
2024-05-28 00:42:51 -0700

    Update main.py
    Adding comments and made changes to the nodes

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+
+#Iterative Method
 def dfs(graph, start, visited=None):
-    if visited is None:
-        visited = set()
-    visited.add(start)
+    if visited is None:#Marking none as visited first
+        visited = set()#Setting some nodes to create an edge
+    visited.add(start)#Starting the code
 
     print(start)
 
@@ -9,10 +11,10 @@ def dfs(graph, start, visited=None):
         dfs(graph, next, visited)
     return visited
 
-graph = {'0': set(['1', '2']),
-         '1': set(['0', '3', '4']),
-         '2': set(['0']),
-         '3': set(['1']),
-         '4': set(['2', '3'])}
+graph = {'0': set(['1', '2', '3']),
+         '1': set(['0', '2']),
+         '2': set(['0','1','4']),
+         '3': set(['0']),
+         '4': set(['2'])}
 
 dfs(graph, '0')
